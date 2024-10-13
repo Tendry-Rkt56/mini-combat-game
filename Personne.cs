f78d903855328d31add5c0a9b334095e26860f62
Tendry <tendryzephyrin@gmail.com>
2024-10-13 22:50:04 +0200

    Création du projet

---
+++ Personne.cs
@@ -0,0 +1,76 @@
+// using system;
+public class Guerrier 
+{
+    //les choses à declarer dans une classe
+    private string Nom;
+    private int Attaque;
+    private int Defense;
+    private int Sante;
+    private int Mana;
+
+    //constructeur
+    public Guerrier (string nom, int attaque, int defense, int sante, int mana)
+    {
+        this.Nom = nom;
+        this.Attaque = attaque;
+        this.Defense  =defense;
+        this.Sante = sante;
+        this.Mana = mana;
+    }
+
+    public Guerrier (string nom,int attaque,int defense,int sante)
+    {
+        this.Nom = nom;
+        this.Attaque = attaque;
+        this.Defense = defense;
+        this.Sante = sante;
+        this.Mana = 100;
+    }
+
+    public void LancerAttaque(Guerrier cible)
+    {
+        if (this.Attaque > cible.Defense) {
+            cible.Sante -= this.Attaque;
+            Console.WriteLine("Resultat du lancement d'attaque:Diminuation du santé");
+        }
+        else if (cible.Sante <= 0) {
+            Console.WriteLine("Resultat du lancement d'attaque:Guerrier Mort");
+        }
+        else {
+            Console.WriteLine("Resultat du lancement d'attaque:sans effet");
+        }
+        Console.WriteLine("le Joueur "+this.Nom+ " attaque le Joueur "+cible.Nom);
+    }
+    public void LancerAttaqueAvecDefense(Guerrier cible)
+    {
+        if(this.Attaque<cible.Defense) {
+            this.Sante -= (cible.Defense-this.Attaque);
+        }
+        else if(this.Attaque>cible.Defense) {
+            cible.Sante -= (this.Attaque-cible.Defense);
+        }
+        else {
+            Console.WriteLine("vous etes à egalité");
+        }
+    }
+
+    public void ActiveSoin()
+    {
+        if (this.Mana < 10) {
+            Console.WriteLine("votre Mana est plus faible");
+        }
+        else {
+            this.Mana -=10;
+            this.Sante+=10;
+            Console.WriteLine("votre mana est:"+this.Mana);
+            Console.WriteLine("Votre niveau de vie est maintenant:"+this.Sante);
+        }
+    }
+    public void AfficheInformation()
+    {
+        Console.WriteLine("Nom: " +this.Nom);
+        Console.WriteLine("Attaqe: " +this.Attaque);
+        Console.WriteLine("Defense: " +this.Defense);
+        Console.WriteLine("Sante: " +this.Sante);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Helloworld
+{
+        class Program
+        {
+            static void Main(string[] args) 
+            {
+                Guerrier pers1 = new Guerrier("Rakoto", 20, 10, 50, 20);
+                Guerrier pers2 = new Guerrier("Rabe", 25, 30, 10);
+                pers1.LancerAttaque(pers2);
+                Console.WriteLine("");
+                pers1.ActiveSoin();
+                pers1.ActiveSoin();
+                pers1.ActiveSoin();
+                Console.WriteLine("");
+                pers1.LancerAttaqueAvecDefense(pers2);
+                Console.WriteLine("Joueur 1");
+                pers1.AfficheInformation();
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Joueur 2");
+                pers2.AfficheInformation();
+            }
+        }
+}
